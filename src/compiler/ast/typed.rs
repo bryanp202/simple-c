4ce6715893f9c789ffc7be2e70f1af3c6aa79919
bryanp24 <bryanp24@att.net>
2026-02-21 19:03:44 -0500

    added bss support for global static variables

--- src/compiler/ast/typed.rs
+++ src/compiler/ast/typed.rs
@@ -137,7 +137,13 @@ impl<'src, 'a, A: Allocator> SymbolTable<'src, 'a, A> {
         }
     }
 
-    pub fn into_parts(self) -> (Vec<Function<'src, 'a, A>>, Vec<GlobalVar<'src>>) {
+    pub fn into_parts(mut self) -> (Vec<Function<'src, 'a, A>>, Vec<GlobalVar<'src>>) {
+        for (name, data) in self.symbols {
+            if data.specifier_flags.symmetric_difference(SpecifierFlags::Static).is_empty() {
+                self.globals.push(GlobalVar { linkage: Linkage::None, name, generation: None, def: None });
+            }
+        }
+
         (self.functions, self.globals)
     }
 
